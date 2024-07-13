658f3bef4176c9cce5d5a5838870e821771936c5
Diyarbek Oralbaev <125976666+Diyarbekoralbaev@users.noreply.github.com>
2024-07-13 23:11:20 +0500

    urls added

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import PortfolioView, PortfolioDetailView
+from .views import PortfolioView, PortfolioDetailView, CategoryView, CategoryDetailView
 
 
 urlpatterns = [
     path('', PortfolioView.as_view(), name='portfolio'),
     path('<int:pk>/', PortfolioDetailView.as_view(), name='portfolio-detail'),
+    path('categories/', CategoryView.as_view(), name='categories'),
+    path('categories/<int:pk>/', CategoryDetailView.as_view(), name='category-detail'),
 ]
